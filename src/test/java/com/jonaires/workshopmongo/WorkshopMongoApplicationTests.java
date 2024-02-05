0a5f01de70ecc600cef821730fea1b95f619f2c6
Jonathan Aires <airesjonathan123@gmail.com>
2024-02-05 18:18:51 -0300

    first commit

---
+++ src/test/java/com/jonaires/workshopmongo/WorkshopMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonaires.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
