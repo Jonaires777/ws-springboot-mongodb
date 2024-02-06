7afbdb8d41d6c32853a17bdd740c8cdfdeada85c
Jonathan Aires <airesjonathan123@gmail.com>
2024-02-06 18:53:39 -0300

    Retrieving an user by Id

--- src/main/java/com/jonaires/workshopmongo/services/UserService.java
+++ src/main/java/com/jonaires/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.jonaires.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jonaires.workshopmongo.domain.User;
 import com.jonaires.workshopmongo.repository.UserRepository;
+import com.jonaires.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,8 @@ public class UserService {
 		return repository.findAll();
 	}
 	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
