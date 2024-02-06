849736364d8e1d9452b4a5bd1d67539c99a271a0
Jonathan Aires <airesjonathan123@gmail.com>
2024-02-06 19:03:46 -0300

    User insert using Post

--- src/main/java/com/jonaires/workshopmongo/services/UserService.java
+++ src/main/java/com/jonaires/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jonaires.workshopmongo.domain.User;
+import com.jonaires.workshopmongo.dtos.UserDTO;
 import com.jonaires.workshopmongo.repository.UserRepository;
 import com.jonaires.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO obj) {
+		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
 }
