ca5c29858a21e41e9bbe1aee23b7fd7395d1b8bb
Jonathan Aires <airesjonathan123@gmail.com>
2024-02-06 19:09:55 -0300

    Deleting a user

--- src/main/java/com/jonaires/workshopmongo/services/UserService.java
+++ src/main/java/com/jonaires/workshopmongo/services/UserService.java
@@ -30,6 +30,11 @@ public class UserService {
 		return repository.insert(obj);
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		repository.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO obj) {
 		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
