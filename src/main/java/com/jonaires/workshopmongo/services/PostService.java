5e8af1a07ea18f57a51ef6b288987c9ae50cbd15
Jonathan Aires <airesjonathan123@gmail.com>
2024-02-08 14:41:17 -0300

    Finding post by id

---
+++ src/main/java/com/jonaires/workshopmongo/services/PostService.java
@@ -0,0 +1,28 @@
+package com.jonaires.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jonaires.workshopmongo.domain.Post;
+import com.jonaires.workshopmongo.repository.PostRepository;
+import com.jonaires.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	public List<Post> findAll(){
+		return repository.findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
